d4baa690c3241dadab43804c4ab99c78e8431acb
Deiver1182 <deiver_velez@hotmail.com>
2016-07-16 10:33:41 -0500

    Se sube la base del proyecto
    Creacion del proyecto

---
+++ app/app.js
@@ -0,0 +1,28 @@
+﻿'used strict';
+
+var aplicacion = angular.module('appPrueba', []);
+
+aplicacion.controller('controlador', function ($scope) {
+    $scope.nombre = "Deiver";
+    $scope.apellido = "Velez Puello";
+
+    $scope.cambiarNombre = function (newNombre, newApellido) {
+        $scope.nombre = newNombre;
+        $scope.apellido = newApellido;
+    };
+
+    $scope.estado = false;
+    $scope.showInput = function () {
+        $scope.estado = !$scope.estado;
+    };
+
+    $scope.listaPersonas = [{ identificacion: '1', nombre: 'Luis' },
+                            { identificacion: '2', nombre: 'Pedro' },
+                            { identificacion: '3', nombre: 'Maria' },
+                            { identificacion: '4', nombre: 'Juan' },
+                            { identificacion: '5', nombre: 'Mario' },
+                            { identificacion: '6', nombre: 'Luisa' },
+                            { identificacion: '7', nombre: 'Alejandra' },
+                            { identificacion: '8', nombre: 'Carmen' }];
+
+    });
